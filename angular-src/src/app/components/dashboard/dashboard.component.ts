4021bb9a95c122495dcdf05c41df809857143040
e1901196 <e1901196@edu.vamk.fi>
2022-07-23 17:33:55 +0300

    started working on dashboard

--- angular-src/src/app/components/dashboard/dashboard.component.ts
+++ angular-src/src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'app/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  user: Object;
+  log: Object;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.authService.getProfile().subscribe(profile => {
+      this.user = profile.user;
+    },
+    err => {
+      console.log(err);
+      return false;
+    }
+    );
+  }
+
+  getLogsForUser(username: String, date1: Date, date2: Date) {
+    // Get all logs first
+    //WIP
+    this.authService.getLogs(username);
   }
 
 }
